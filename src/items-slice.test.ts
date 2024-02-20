a18ec8811d3fd2bd6badd53953b72840871c26f4
dzungvu <dung.vu@4digit.jp>
2024-02-20 17:49:03 +0700

    adding test

---
+++ src/items-slice.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+
+/**
+ * toBe: https://vitest.dev/api/expect.html#tobe
+ * toBeCloseTo: https://vitest.dev/api/expect.html#tobecloseto
+ * toBeInstanceOf: https://vitest.dev/api/expect.html#tobeinstanceof
+ * toBeUndefined: https://vitest.dev/api/expect.html#tobeundefined
+ * toContain: https://vitest.dev/api/expect.html#tocontain
+ * toThrow: https://vitest.dev/api/expect.html#tothrow
+ * toThrowError: https://vitest.dev/api/expect.html#tothrowerror
+ */
+
+it("should pass if the two numbers would add up correctly in a language other than JavaScript", () => {
+  expect(0.2 + 0.1).toBeCloseTo(0.3);
+});
